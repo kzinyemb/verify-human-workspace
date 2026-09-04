dd627a34231b330a41684e583e33d1fcf175324d
kzinyemb <kzinyemb@gmail.com>
2026-09-04 13:47:27 -0400

    Hardcode Clerk production key

--- src/Editorial-main.tsx
+++ src/Editorial-main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import EditorialApp from './EditorialApp';
 import { ClerkProvider, SignedIn, SignedOut, SignIn } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = "pk_live_Y2xlcmsudmVyaWZ5LWh1bWFuLXdvcmtzcGFjZS5wYWdlcy5kZXYk";
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
